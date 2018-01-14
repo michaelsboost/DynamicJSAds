f34481e2cf5211ba579f8bb58941ca9d7d1ac84e
Michael <mikethedj4@yahoo.com>
2018-01-13 20:05:55 -0600

    Customized Ads

--- ads.js
+++ ads.js
@@ -2,10 +2,11 @@ var ads = [
   '<a href="https://grantcardone.com/?rfsn=853345.4464e" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/cardoneshop.jpg" style="max-width: 728px; width: 100%;"></a>',
   '<a href="https://grantcardone.com/products/grant-cardone-sales-training-university-2?rfsn=853345.4464e" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/cardoneuniversity.jpg" style="max-width: 728px; width: 100%;"></a>',
   '<a href="https://www.amazon.com/gp/gc?&tag=mikethedj4-20&camp=212333&creative=380405&linkCode=ur1&adid=1Y6QK5ZMYH1ZA2P9HXF0&" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/amazongiftcards.png" style="max-width: 728px; width: 100%;"></a>',
-  '<a href="https://www.paypal.com/us/webapps/mpp/invite?token=kepXqLE2LN0" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/paypal.png" style="max-width: 728px; width: 100%;"></a>',
-  '<a href="https://cash.me/app/HMRRQXC" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/squarecash.png" style="max-width: 728px; width: 100%;"></a>',
-  '<a href="https://www.taskrabbit.com/r/360a3/try" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/taskrabbit.png" style="max-width: 728px; width: 100%;"></a>',
-  '<a href="https://get.uber.com/invite/michaels41699ue" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/uber.png" style="max-width: 728px; width: 100%;"></a>'
+  '<a href="http://www.swagbucks.com/refer/justicerises" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/swagbucks.jpg" style="max-width: 728px; width: 100%;"></a>',
+  '<a href="https://www.paypal.com/us/webapps/mpp/invite?token=kepXqLE2LN0" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/paypal.svg" style="max-width: 728px; width: 100%;"></a>',
+  '<a href="https://cash.me/app/HMRRQXC" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/squarecash.svg" style="max-width: 728px; width: 100%;"></a>',
+  '<a href="https://www.taskrabbit.com/r/360a3/try" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/taskrabbit.svg" style="max-width: 728px; width: 100%;"></a>',
+  '<a href="https://get.uber.com/invite/michaels41699ue" target="_blank"><img src="https://michaelsboost.github.io/DynamicJSAds/images/uber.svg" style="max-width: 728px; width: 100%;"></a>'
 ]
 
 var randomNumber = Math.floor(Math.random() * (ads.length));
